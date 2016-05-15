d741dcd1a5c742fb9645185d5812b4b546d61c2d
Ernest Mushinge <emushinge@gmail.com>
2016-05-15 10:29:44 -0400

    Update First_Java_Application.java
    Check out the changes I made

--- Chapter_1/First_Java_Application.java
+++ Chapter_1/First_Java_Application.java
@@ -3,5 +3,7 @@ public class First_Java_Application
 	public static void main(String[] args)
 	{
 		System.out.println("First Java application");
+		//this is the change I made (By Ernest Mushinge)
+		Sytem.out.println("Data Structure is more fun" + "You should try singly Linked List");
 	}
 }
